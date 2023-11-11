12fe80d68c4b1c1569f401e689f5b31aa2faddb3
pulna <natasyo23@gmail.com>
2023-11-11 23:52:30 +0300

    fix: trending

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default async function Home() {
 
       <PopularCategories />
 
-      {/*<Trending />*/}
+      <Trending />
       {/*<Sale />*/}
       <AppMobile />
       {/*<Info />*/}
